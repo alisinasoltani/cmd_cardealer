ff7edf42624b77592dbbd1755935d32437b379dc
Alisina <alisinasoltani82@gmail.com>
2024-11-20 16:20:21 +0330

    feat!: item class header created

---
+++ Classes/Item.h
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#ifndef __Item__
+    #define __Item__
+    class Item {
+        protected:
+            std::string name;
+            std::string model;
+            std::string manufacturer;
+            std::time_t date_of_creation;
+
+        public:
+            Item(std::string name, std::string model, std::string manufacturer, std::time_t date_of_creation);
+            Item(const Item& item);
+            std::string getName() const;
+            std::string getModel() const;
+            std::string getManufacturer() const;
+            std::string getDateOfCreation() const;
+            void virtual show_info() const = 0;
+    };
+#endif
---
+++ makefile
@@ -0,0 +1,2 @@
+make:
+	g++ -o app.exe
