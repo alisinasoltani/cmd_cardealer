fbfee4b824dee14925c78a74cd5320c241454324
Alisina <alisinasoltani82@gmail.com>
2024-11-23 14:22:34 +0330

    feat!: bicycle class header created

---
+++ Classes/Bicycle.h
@@ -0,0 +1,15 @@
+#include "Rent.h"
+#include "Vehicle.h"
+#include <iostream>
+#include <chrono>
+
+#ifndef __BICYCLE__
+    #define __BICYCLE__
+    class Motorcycle: public Rent, Vehicle {
+        public:
+            Motorcycle(std::string name, std::string model, std::string manufacturer, std::time_t date_of_creation, double rent_price, int max_speed):
+            Rent(name, model, manufacturer, date_of_creation, rent_price, true), Vehicle(max_speed) {}
+
+            void drive() override;
+    };
+#endif
